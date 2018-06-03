a1c527ff6d4559cefc61c280671e23c1e9d9fb3d
John Thummel <enikath@gmail.com>
2018-06-03 11:50:05 -0500

    first

---
+++ TodoAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoAPI.Controllers
+{
+    [Route("api/FileUpload")]
+    public class FileUploadController : Controller
+    {
+    }
+}
